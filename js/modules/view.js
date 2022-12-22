528f194a95e3d971ea2f0514b99951349af1f2d6
Estevam Neto <estevam.neto07@gmail.com>
2022-12-21 23:50:56 -0300

    em andamento

---
+++ js/modules/view.js
@@ -0,0 +1,15 @@
+export default function initView() {
+
+}
+
+// const visibility = document.querySelectorAll('.visibility');
+// const view = document.querySelectorAll('.view');
+
+
+// visibility.forEach(item => {
+//     item.addEventListener('mouseover', () => {
+//      view.forEach(ver => {
+//         ver.classList.toggle('active');
+//      });
+//     });
+// });
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initBackToTop from "./modules/back-to-top.js"; 
 import initTecHover from "./modules/tec-hover.js";
+import initView from "./modules/view.js";
 
 initBackToTop();
 initTecHover();
+initView();
