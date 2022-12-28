d0ddb62253358bb6a4a9cba2cd8a6f6d277e6d96
Estevam Neto <estevam.neto07@gmail.com>
2022-12-28 13:27:46 -0300

    att

---
+++ js/modules/view.js
@@ -0,0 +1,20 @@
+export default function initView() {
+
+}
+
+// const visibility = document.querySelectorAll('.visibility');
+// const view = document.querySelectorAll('.view');
+
+// function mouseView(event, index) {
+//     console.log(event, index)
+//     view.forEach(icon => {
+//         icon.classList.add('active');
+//     })
+
+// }
+
+// visibility.forEach((item, index) => {
+//     item.addEventListener('mouseover', () => {
+//         mouseView(index)
+//     });
+// });
